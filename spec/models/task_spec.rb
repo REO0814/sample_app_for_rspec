b7527a275273dfda8a095a0b8072dc86461f4167
Reo Motegi <mtgrooo@gmail.com>
2020-12-16 14:09:27 +0900

    [Fix] task_spec.rbでサンプルデータをFactoryBotを使用したリファクタリング

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,52 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  let(:user) { user = FactoryBot.create(:user) }
+  let(:user) { user = create(:user) }
   describe 'validation' do
     it 'is valid with all attributes' do
-      task = Task.new(
-        title: 'test',
-        content: 'test content',
-        status: 0,
-        deadline: Date.current.in_time_zone,
-        user: user
-      )
+      task = build(:task)
       expect(task).to be_valid
     end
     
     it 'is invalid without title' do
-      task = Task.new(title: nil)
-      task.valid?
-      expect(task.errors[:title]).to include("can't be blank")
+      task_without_title = build(:task, title: "")
+      task_without_title.valid?
+      expect(task_without_title.errors[:title]).to include("can't be blank")
     end
     
     it 'is invalid without status' do
-      task = Task.new(status: nil)
-      task.valid?
-      expect(task.errors[:status]).to include("can't be blank")
+      task_without_status = build(:task, status: "")
+      # task = Task.new(status: nil)
+      task_without_status.valid?
+      expect(task_without_status.errors[:status]).to include("can't be blank")
     end
     
     it 'is invalid with a duplicate title' do
-      task = FactoryBot.create(:task)
-      other_task = Task.new(
-        title: 'title',
-        content: 'content',
-        status: 0,
-        user: user
-      )
-      other_task.valid?
-      expect(other_task.errors[:title]).to include("has already been taken")
+      task = create(:task)
+      task_with_duplicated_title = build(:task, title: task.title)
+      task_with_duplicated_title.valid?
+      expect(task_with_duplicated_title.errors[:title]).to include("has already been taken")
     end
     
     it 'is valid with another title' do
-      task = FactoryBot.create(:task)
-      other_task = Task.new(
-        title: 'another title',
-        content: 'content',
-        status: 0,
-        user: user
-      )
-      expect(other_task).to be_valid
+      task = create(:task)
+      task_with_another_title = build(:task, title: 'another title')
+      expect(task_with_another_title).to be_valid
     end
   end
 end
